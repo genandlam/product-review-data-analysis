89cedac71ff6ece90c9df2ef6c28cd0321bf51b3
gudgud96 <helloharry66@gmail.com>
2018-10-26 22:54:18 +0800

    update commit

--- Topic Modelling/noun-phrase-detector.py
+++ Topic Modelling/noun-phrase-detector.py
@@ -1,6 +1,6 @@
 import json
 from textblob import TextBlob
-from textblob.en.taggers import NLTKTagger
+from textblob.en.taggers import NLTKTagger, PatternTagger
 from textblob.np_extractors import ConllExtractor
 import nltk
 
@@ -9,16 +9,22 @@ import nltk
  ( ) - Revise the regex pattern
 '''
 def main():
-    FILENAME = "CellPhoneReview-1000.json"
-    print('Reading data...')
-    review_data = open(FILENAME).readlines()
-    document = [json.loads(d)['reviewText'] for d in review_data][4]
+    # FILENAME = "CellPhoneReview-1000.json"
+    # print('Reading data...')
+    # review_data = open(FILENAME).readlines()
+    # document = [json.loads(d)['reviewText'] for d in review_data][0]
+    document = "These are awesome and make my phone look so stylish! I have only used one so far and have had it on for almost a year! CAN YOU BELIEVE THAT! ONE YEAR!! Great quality!"
     print(document)
     nltk_tagger = NLTKTagger()
     extractor = ConllExtractor()
     blob = TextBlob(document, pos_tagger=nltk_tagger, np_extractor=extractor)
     print(blob.tags)
     print(blob.noun_phrases)
+
+    pattern_tagger = PatternTagger()
+    blob2 = TextBlob(document, pos_tagger=pattern_tagger, np_extractor=extractor)
+    print(blob2.tags)
+    print(blob2.noun_phrases)
 
     tagged = nltk.pos_tag(tokenize(document.lower()))
     print(tagged)
